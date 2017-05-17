fc52600baad9aab6d22020257d33354465bb2de2
Siwei Xu <xusiwei1236@163.com>
2017-05-17 10:13:40 +0800

    enable clock for GPIO port registers.

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void SystemInit()
 void delay_ms(int ms)
 {
 	while (ms--) {
-		int ticks = 12*1000;
+		int ticks = 2*1000;
 		while (ticks--);
 	}
 }
@@ -16,30 +16,28 @@ int main()
 {
 	LPC_SYSCON->SYSAHBCLKCTRL |= (1<<16); // Enables clock for I/O configuration block.
 	// set pin functions
-	LPC_IOCON->TDI_PIO0_11 &= ~0x1F; // clear bits [4:0]
-	LPC_IOCON->TDI_PIO0_11 |= 0x1; // set bits[2:0] as 0x1, PIO0_11 function.
-	LPC_IOCON->TDI_PIO0_11 |= (0x1 << 2); // set bits[4:3] as 0x1, Pull-down mode.
+	LPC_IOCON->PIO0_20 &= ~0x1F; // clear bits [7:0]
+	LPC_IOCON->PIO0_20 |= 0u; // set bits[2:0] as PIO0_20 function.
+	LPC_IOCON->PIO0_20 |= (0u << 2); // set bits[4:3] as Inactive mode.
+	LPC_IOCON->PIO0_20 |= (1u << 10); // Open-drain mode.
 	
-	LPC_IOCON->PIO0_20 &= ~0x01F; // clear bits [4:0]
-	LPC_IOCON->PIO0_20 |= 0x0; // set bits[2:0] as PIO0_20 function.
-	LPC_IOCON->PIO0_20 |= (0x0 << 2); // set bits[4:3] as Inactive mode.
-	
-	LPC_IOCON->PIO0_21 &= ~0x1F; // clear bits [4:0]
-	LPC_IOCON->PIO0_21 |= 0x0; // set bits[2:0] as PIO0_21 function.
-	LPC_IOCON->PIO0_21 |= (0x1 << 2); // set bits[4:3] as Pull-down mode.
+	LPC_IOCON->PIO0_21 &= ~0x1F; // clear bits [7:0]
+	LPC_IOCON->PIO0_21 |= 0u; // set bits[2:0] as PIO0_21 function.
+	LPC_IOCON->PIO0_21 |= (0u << 2); // set bits[4:3] as Inactive mode.
+	LPC_IOCON->PIO0_21 |= (1u << 10); // Open-drain mode.
 
 	LPC_SYSCON->SYSAHBCLKCTRL &= ~(1<<16); // Disables clock for I/O configuration block.
 
+	LPC_SYSCON->SYSAHBCLKCTRL |= (1<<6); // Enables clock for GPIO port registers.
+
 	// set GPIO directions
-	LPC_GPIO->DIR[0] |= (1<<11); // PIO0_11 for output
-	LPC_GPIO->DIR[0] |= (1<<20); // PIO0_20 for output
-	LPC_GPIO->DIR[0] |= (1<<21); // PIO0_21 for output
+	LPC_GPIO->DIR[0] |= (1u << 20); // PIO0_20 for output
+	LPC_GPIO->DIR[0] |= (1u << 21); // PIO0_21 for output
 
-	// PIO0_11, PIO0_20, PIO0_21
-	LPC_GPIO->B0[11] = 0x0;
-	LPC_GPIO->B0[21] = 0x0;
-
+	// PIO0_20, PIO0_21
 	while (1) {
+		LPC_GPIO->SET[0] |= (1u << 21); delay_ms(1000);
+		LPC_GPIO->CLR[0] |= (1u << 21); delay_ms(1000);
 		LPC_GPIO->B0[20] ^= 0x1; // flip bit
 		delay_ms(1000);
 	}
